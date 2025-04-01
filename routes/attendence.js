97767535b64e5a8fd9dccc98a17ecacc089a7566
shubham <shubhamshiware08@gmail.com>
2025-04-01 16:34:08 +0530

    ff

--- routes/attendence.js
+++ routes/attendence.js
@@ -3,6 +3,7 @@ const Attendance = require("../model/attendence");
 const express = require("express");
 // const { verifytoken } = require("../middleware/midd");
 const mongoose = require("mongoose");
+// const authMiddleware= require("../middleware/authmiddleware")
 const router = express.Router();
 const {
   getData,
@@ -52,6 +53,22 @@ router.post("/attendance", async (req, res,id) => {
   res.json({ message: "Attendance marked successfully" ,
     userid:id
   });
+});
+
+router.get("/:userId",  async (req, res) => {
+    try {
+        const { userId } = req.params;
+        if (!mongoose.Types.ObjectId.isValid(userId)) {
+            return res.status(400).json({ message: "Invalid user ID" });
+        }
+
+        const attendanceRecords = await Attendance.find({ userId }).sort({ date: -1 });
+
+        res.status(200).json(attendanceRecords);
+    } catch (error) {
+        console.error("Error fetching attendance:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 });
 
 module.exports = router;
