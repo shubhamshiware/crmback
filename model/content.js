9af48ade2d86b0adc409d0ec8e1cf761d0f51d32
shubham <shubhamshiware08@gmail.com>
2025-04-07 13:15:06 +0530

    ff

--- model/content.js
+++ model/content.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const contentSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   update: String,
+  discription:String,
   completed: { type: Boolean, default: false },
   points: { type: Number, default: 0 },
   uploadedAt: { type: Date, default: Date.now },
