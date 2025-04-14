f705d0bf50d266ebebacb2771158b386301a5c05
shubham <shubhamshiware08@gmail.com>
2025-04-14 17:21:51 +0530

    dd

--- middleware/protect.js
+++ middleware/protect.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 const protect = async (req, res, next) => {
-  let token;
-  console.log(token, "token");
+  console.log(req.body, "token");
+  let token = req.body;
 
   if (
     req.headers.authorization &&
