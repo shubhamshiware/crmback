69b93daa3417bb83c5fe3b79f4463b12967e1ef7
shubham <shubhamshiware08@gmail.com>
2025-04-17 19:21:29 +0530

    ff

--- middleware/protect.js
+++ middleware/protect.js
@@ -3,7 +3,6 @@ const User = require("../model/user");
 require("dotenv").config(); // make sure this is loaded
 
 const protect = async (req, res, next) => {
-  console.log(req.body, "req");
   let token;
 
   if (
@@ -14,14 +13,14 @@ const protect = async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
       console.log(token, "token from frontend");
 
-      // ✅ Use the secret key here
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       console.log(decoded, "secret key");
 
       // 👇 Attach the user to request object
       req.user = await User.findById(decoded.id).select("-password");
 
-      return next(); // allow access
+      return next();
+      s;
     } catch (error) {
       console.error("Token verification failed:", error.message);
       return res.status(401).json({ message: "Not authorized, token failed" });
