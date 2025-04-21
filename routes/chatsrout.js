e79e832dbb89b070cff8d018e2e7fb79e585ac45
shubham <shubhamshiware08@gmail.com>
2025-04-21 13:16:13 +0530

    ff

--- routes/chatsrout.js
+++ routes/chatsrout.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { accessChat } = require("../controller/chats");
-const { sendMessage, getMessages } = require("../controller/messages");
+// const { sendMessage, getMessages } = require("../controller/messages");
 
-router.post("/ms", sendMessage);
-router.get("/:chatId", getMessages);
+// router.post("/ms", sendMessage);
+// router.get("/:chatId", getMessages);
 
-router.post("/", accessChat);
+router.post("/chats", accessChat);
 
 module.exports = router;
