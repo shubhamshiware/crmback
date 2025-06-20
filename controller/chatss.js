52babe3747e798dc13cca2ee6371f1958aad4ec1
shubham <shubhamshiware08@gmail.com>
2025-06-20 14:58:12 +0530

    dd

---
+++ controller/chatss.js
@@ -0,0 +1,90 @@
+const Chat = require("../models/Chat");
+const User = require("../models/User");
+
+// Create or fetch one-to-one chat
+const accessChat = async (req, res) => {
+  const { userId } = req.body;
+
+  if (!userId) {
+    console.log("❌ User ID not sent with request");
+    return res.status(400).send("User ID required");
+  }
+
+  try {
+    // Check if chat already exists
+    let chat = await Chat.findOne({
+      isChatGroup: false,
+      users: { $all: [req.user._id, userId] },
+    })
+      .populate("users", "-password")
+      .populate("latestMessage");
+
+    if (chat) {
+      return res.status(200).json(chat);
+    }
+
+    // Create new chat
+    const newChat = new Chat({
+      chatName: "sender",
+      isChatGroup: false,
+      users: [req.user._id, userId],
+    });
+
+    const createdChat = await newChat.save();
+
+    const fullChat = await Chat.findById(createdChat._id).populate(
+      "users",
+      "-password"
+    );
+
+    res.status(201).json(fullChat);
+  } catch (error) {
+    console.error("Error creating chat:", error);
+    res.status(500).send("Server error");
+  }
+};
+
+// Create a new group chat
+const createGroupChat = async (req, res) => {
+  const { name, users } = req.body;
+
+  if (!name || !users) {
+    return res
+      .status(400)
+      .json({ message: "Please provide group name and users" });
+  }
+
+  const parsedUsers = JSON.parse(users); // because frontend usually sends JSON string
+
+  if (parsedUsers.length < 2) {
+    return res
+      .status(400)
+      .json({ message: "At least 2 users required to form a group chat" });
+  }
+
+  parsedUsers.push(req.user); // include the creator
+
+  try {
+    const groupChat = await Chat.create({
+      chatName: name,
+      users: parsedUsers,
+      isChatGroup: true,
+      groupAdmin: req.user._id,
+    });
+
+    const fullGroupChat = await Chat.findById(groupChat._id)
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+    res.status(201).json(fullGroupChat);
+  } catch (error) {
+    console.error("Error creating group chat:", error);
+    res.status(500).send("Server error");
+  }
+};
+
+module.exports = {
+  accessChat,
+
+  createGroupChat,
+};
