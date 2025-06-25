7c6917916e4b4916fd2aa76f53f0b8a4552658e1
shubham <shubhamshiware08@gmail.com>
2025-06-25 13:27:31 +0530

    dd

--- controller/chats.js
+++ controller/chats.js
@@ -1,7 +1,6 @@
 const Chat = require("../model/chat");
 
 const accessChat = async (req, res) => {
-  cosnoel.log(req.body, "aa");
   if (!userId1 || !userId2) {
     return res.status(400).json({ message: "Missing user IDs" });
   }
@@ -28,31 +27,11 @@ const accessChat = async (req, res) => {
 };
 
 const getAllChats = async (req, res) => {
-  console.log(req.body, "incomming request");
   try {
     const chats = await Chat.find().populate("users", "-password");
     res.json(chats);
   } catch (error) {
     res.status(500).json({ message: error.message });
-  }
-};
-
-const groupchats = async (req, res) => {
-  console.log("request from frontend", req.body);
-  try {
-    const grpChats = await Chat.find().populate("group", "-password");
-    const user = chats.id.find().populate("user", "password");
-    res.json(grpChats);
-  } catch (error) {
-    res.status(505).json({
-      message: error.message,
-    });
-  }
-};
-
-const access = async (req, res) => {
-  if (!useroId1 || !userId2) {
-    return res.status(500).json({});
   }
 };
 
